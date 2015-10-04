b82c62ac36cab335f596bbbaa254d70e505f72ea
reymargera <javierv2@uci.edu>
2015-10-03 23:47:27 -0700

    adding all files

---
+++ app.js
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2014 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var express = require('express'),
+  app = express(),
+  errorhandler = require('errorhandler'),
+  bluemix = require('./config/bluemix'),
+  watson = require('watson-developer-cloud'),
+  extend = require('util')._extend,
+  fs = require('fs'),
+  dummy_text = fs.readFileSync('mobydick.txt');
+
+
+// Bootstrap application settings
+require('./config/express')(app);
+
+// if bluemix credentials exists, then override local
+var credentials = extend({
+    version: 'v2',
+    url: 'https://gateway.watsonplatform.net/personality-insights/api',
+    username: 'c1554254-f4e5-46be-976d-3970dffb0cf4',
+    password: '4HiLYUAizzk4'
+}, bluemix.getServiceCreds('personality_insights')); // VCAP_SERVICES
+
+var personalityInsights = new watson.personality_insights(credentials);
+
+var credentialsBackup = {
+  url: 'https://stream.watsonplatform.net/text-to-speech/api',
+  version: 'v1',
+  username: '<username>',
+  password: '<password>',
+  use_vcap_services: true   
+};
+
+var credentials2 = extend(credentialsBackup, bluemix.getServiceCreds('text_to_speech'));
+// Create the service wrapper
+var textToSpeech = watson.text_to_speech(credentials2);
+var authorization = watson.authorization(credentials2);
+
+app.use(express.static('./public'));
+
+// render index page
+app.get('/', function(req, res) {
+  res.render('index', { content: dummy_text });
+});
+
+app.post('/', function(req, res) {
+  personalityInsights.profile(req.body, function(err, profile) {
+    if (err) {
+      if (err.message){
+        console.log(credentials);
+        err = { error: err.message };
+      }
+      return res.status(err.code || 500).json(err || 'Error processing the request');
+    }
+    else
+      return res.json(profile);
+  });
+});
+
+// Get token from Watson using your credentials
+app.get('/token', function(req, res) {
+  authorization.getToken({url: credentials.url}, function(err, token) {
+    if (err) {
+      console.log('error:', err);
+      res.status(err.code);
+    }
+
+    res.send(token);
+  });
+});
+
+app.get('/synthesize', function(req, res) {
+  var transcript = textToSpeech.synthesize(req.query);
+  transcript.on('response', function(response) {
+    if (req.query.download) {
+      response.headers['content-disposition'] = 'attachment; filename=transcript.ogg';
+    }
+  });
+  transcript.on('error', function(error) {
+    console.log('Synthesize error: ', error)
+  });
+  transcript.pipe(res);
+});
+
+
+// Add error handling in dev
+if (!process.env.VCAP_SERVICES) {
+  app.use(errorhandler());
+}
+
+var port = process.env.VCAP_APP_PORT || 3000;
+app.listen(port);
+console.log('listening at:', port);
---
+++ instructions.md
@@ -0,0 +1,24 @@
+Get started with PersonalStatemenHelper
+-----------------------------------
+Welcome to your new Personality Insights Node.js app!
+
+Node.js app runs on a server-side high performance, highly scalable, event-driven environment with non-blocking I/O that is programmed with the JavaScript programming language.
+
+1. [Install the cf command-line tool](https://www.ng.bluemix.net/docs/#starters/BuildingWeb.html#install_cf).
+2. [Download the starter application package](https://console-classic-20151002-205310.ng.bluemix.net:443/rest/../rest/apps/b353e427-d773-4a6a-8083-c0af6b849345/starter-download).
+3. Extract the package and `cd` to it.
+4. Connect to Bluemix:
+
+		cf api https://api.ng.bluemix.net
+
+5. Log into Bluemix:
+
+		cf login -u javierv2@uci.edu
+		cf target -o javierv2@uci.edu -s dev
+
+6. Deploy your app:
+
+		cf push PersonalStatemenHelper
+
+7. Access your app: [PersonalStatemenHelper.mybluemix.net](//PersonalStatemenHelper.mybluemix.net)
+
